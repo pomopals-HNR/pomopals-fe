0da0e9cbf266ebc446f9b646b1fea2233d887e85
tiff <nottiffchan@gmail.com>
2022-01-08 00:44:24 +0800

    Update fonts setup

--- src/theme.js
+++ src/theme.js
@@ -10,7 +10,7 @@ const fontStyleOverrides = `@font-face {
           font-style: normal;
           font-display: swap;
           font-weight: 400;
-          src: local('CerebriSans'), local('CerebriSans-Book'), url(${CerebriSansBook}) format('ttf');
+          src: url(${CerebriSansBook}) format('ttf');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
 
@@ -174,9 +174,9 @@ const typography = {
 export const theme = createTheme({
   palette: palette,
   typography: typography,
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: fontStyleOverrides,
-    },
-  },
+  // components: {
+  //   MuiCssBaseline: {
+  //     styleOverrides: fontStyleOverrides,
+  //   },
+  // },
 });
