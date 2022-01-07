f220dea500f4054cc529e26c49514e4631de1b55
tiff <nottiffchan@gmail.com>
2022-01-07 12:06:00 +0800

    Navbar component

--- src/App.js
+++ src/App.js
@@ -4,67 +4,14 @@ import * as React from "react";
 import { theme } from "./theme";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { URI } from "./global";
+import Reference from "./pages/Reference";
 
 function App() {
-  const [userId, setUserId] = useState(localStorage.userId);
-
-  const [currentUser, setCurrentUser] = useState(null);
-
-  useEffect(() => {
-    if (userId) {
-      axios
-        .get(`${URI}/users/${userId}`)
-        .then((res) => {
-          setCurrentUser(res.data[0]);
-          localStorage.setItem("name", res.data[0].name);
-        })
-        .catch(function (error) {
-          if (error.response) {
-            console.log(error.response.data);
-          } else if (error.request) {
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-          }
-          //setPending(false);
-          console.log(error.config);
-        });
-    } else {
-      setCurrentUser(null);
-    }
-  }, [userId]);
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Navbar currentUser={currentUser} onUpdate={setUserId} />
-      <div
-        style={{
-          padding: "3em",
-          backgroundColor: theme.palette["yellow"].active,
-        }}
-      >
-        MUI uses rem units for the font size. The browser element default font
-        size is 16px, but browsers have an option to change this value, so rem
-        units allow us to accommodate the user's settings, resulting in a better
-        accessibility support. Users change font size settings for all kinds of
-        reasons, from poor eyesight to choosing optimum settings for devices
-        that can be vastly different in size and viewing distance.
-      </div>
-      {currentUser && (
-        <div
-          style={{
-            padding: "3em",
-            backgroundColor: theme.palette["purple"].active,
-          }}
-        >
-          Hello, {currentUser.name}
-        </div>
-      )}
+      <Navbar />
+      <Reference />
     </ThemeProvider>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,29 @@
 import { URI } from "../global";
+import logo from "../assets/darkModeLogo.svg";
+import { theme } from "../theme";
+import styled from "@emotion/styled";
+import Button from "./Button";
 import GoogleLogin from "react-google-login";
 
-import { useState, useEffect } from "react";
+const StyledNavbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.7vw 2vw 0 2vw;
+  background-color: ${theme.palette.grey[900]};
+  .links a {
+    color: #fff;
+    margin-left: 40px;
+  }
+  .links Button {
+    margin-left: 40px;
+    font-size: 18px;
+    font-family: "CerebriSans";
+    font-weight: 400;
+  }
+`;
 
-export default function Navbar(props) {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    setUser(props.currentUser);
-  }, [props]);
-
+export default function Navbar() {
   const handleLogin = async (googleData) => {
     const res = await fetch(`${URI}/google-login`, {
       method: "POST",
@@ -22,7 +36,6 @@ export default function Navbar(props) {
     });
     const data = await res.json();
     console.log(data);
-    props.onUpdate(data);
   };
 
   const handleFailure = (googleData) => {
@@ -30,22 +43,30 @@ export default function Navbar(props) {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        padding: "16px 24px",
-        backgroundColor: "lightgray",
-      }}
-    >
-      <h3>pomopals</h3>
-      <GoogleLogin
+    <StyledNavbar>
+      <a href="/" style={{ width: "12vw" }}>
+        <img alt="logo" src={logo} />
+      </a>
+
+      <div className="links">
+        <a href="/">about</a>
+        <a href="/">how it works</a>
+        <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText="Login with Google"
+          onSuccess={handleLogin}
+          onFailure={handleFailure}
+          cookiePolicy={"single_host_origin"}
+          render={(renderProps) => <Button currTheme="dark">log in</Button>}
+        />
+      </div>
+      {/* <GoogleLogin
         clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
         buttonText="Login with Google"
         onSuccess={handleLogin}
         onFailure={handleFailure}
         cookiePolicy={"single_host_origin"}
-      />
-    </div>
+      /> */}
+    </StyledNavbar>
   );
 }
